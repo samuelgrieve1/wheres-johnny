0979609d5c4c70c0b306280ef49808d999be8be8
Sam Grieve <samuel.grieve@icloud.com>
2020-11-10 20:43:44 -0500

    WIP

--- app/javascript/react/components/Home.js
+++ app/javascript/react/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import JohnMap2 from './JohnMap2.js'
+import JohnMap from './JohnMap.js'
+import SearchBox from './SearchBox.js'
 
 export const Home = (props) => {
   return (
@@ -7,11 +8,12 @@ export const Home = (props) => {
       <h1 className="page-heading">Johnny Map</h1>
       <div className="grid-x search-and-map-container">
         <div className="cell small-12 medium-4 large-3 search-container">
-          <label>Search Map
+          {/* <label>Search Map
             <input type="text" placeholder="Address, City or Zip"></input>
-          </label>
+          </label> */}
+          <SearchBox />
         </div>
-        <div className="cell small-12 medium-8 large-9 map-container"><JohnMap2 /></div>
+        <div className="cell small-12 medium-8 large-9 map-container"><JohnMap /></div>
       </div>
     </div>
   )
